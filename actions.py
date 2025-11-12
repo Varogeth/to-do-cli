695b89920cc9f59bb09c4b0cf3032a13f4cbd0d7
Varogeth <varogeth@gmail.com>
2025-11-12 16:58:28 -0500

    Fixed broken id_counter in tasks.json

--- actions.py
+++ actions.py
@@ -3,10 +3,10 @@ def list(tasklist):
         print(f"{task["id"]}. {task["label"]}")
 
 
-def add(tasklist, inc, task):
-    inc += 1
-    new_task = {"id": inc, "label": task}
-    tasklist.append(new_task)
+def add(data, task):
+    data["id_counter"] += 1
+    new_task = {"id": data["id_counter"], "label": task}
+    data["tasks"].append(new_task)
 
 def delete(tasklist, id):
     for task in tasklist:
--- app.py
+++ app.py
@@ -7,10 +7,10 @@ def main():
     action = argv[1]
 
     db = data.load_data()
-    id_counter = db["id_counter"]
+    # id_counter = db["id_counter"]
     tasklist = db["tasks"]
 
-    if action == "add": actions.add(tasklist, id_counter, argv[2])
+    if action == "add": actions.add(db, argv[2])
     elif action == "list": actions.list(tasklist)
     elif action == "delete": actions.delete(tasklist, int(argv[2]))
     else: print(f"Invalid command [{action}]:\nPlease enter a valid command! (add, list, or delete)")
